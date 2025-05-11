8a9e5f9671799011b07b44ca6792d59acd4f0a11
T. Jaisohn Kim <tjkim.main@gmail.com>
2025-05-11 12:51:04 -0400

    Add files via upload

---
+++ 07 Advanced Data Analysis/startup.m
@@ -0,0 +1,17 @@
+% Figure position
+set(groot,'DefaultFigurePosition',[1.1 0.2 0.9 0.65]*1e3)
+
+% Plot aesthetics
+set(groot,'DefaultLineLineWidth',2)
+set(groot,'DefaultAxesXLimitMethod','padded')
+set(groot,'DefaultAxesYLimitMethod','padded')
+set(groot,'DefaultAxesZLimitMethod','padded')
+set(groot,'DefaultAxesXGrid','on')
+set(groot,'DefaultAxesYGrid','on')
+set(groot,'DefaultAxesZGrid','on')
+
+% Text aesthetics
+set(groot,'DefaultAxesFontSize',14)
+set(groot,'DefaultAxesTickLabelInterpreter','latex')
+set(groot,'DefaultTextInterpreter','latex')
+set(groot,'DefaultLegendInterpreter','latex')
